3ecdbcc56ca695aa326b979d6d98334cab1e48c3
edmeister <koed.dev@gmail.com>
2025-05-01 06:12:18 -0700

    a:d

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,6 +1,6 @@
 def test_google_search(browser):
     browser.get("https://www.google.com")
-    assert "Google" in browser.title
+    assert "Googlee" in browser.title
 
 
 # poetry env use python3
